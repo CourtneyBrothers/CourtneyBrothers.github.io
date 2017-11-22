eb2f1911514252b976694952ec2599217cf24ada
Courtney Brothers <cournteywbrothers@gmail.com>
2017-11-21 20:44:37 -0600

    add cards to home page in lieu of article HTML styling

---
+++ second.js
@@ -0,0 +1,42 @@
+resumeArticles = [
+    {
+        title: "Freelance graphic artist",
+        dates:"2016-present",
+        content:"m. Ut gravida urna dolor, nec ornare lorem mollis sit amet. Curabitur vel mi ac nunc porta pretium. In vestibulum laoreet fermentum. Etiam magna orci, feugiat eget venenatis mollis, pulvinar et libero. Duis dolor nunc, vehicula eu lectus vel, interdum venenatis felis. Maecenas eu semper lacus.",
+        
+
+    },{
+        title:"Artist Director of House of Bread",
+        dates:"2016-present",
+        content:"“A man's work is nothing but this slow trek to rediscover, through the detours of art, those two or three great and simple images in whose presence his heart first opened.― Albert Camus",
+        
+
+    },{
+        title:"Title 3",
+        dates:"December 2017",
+        content:"more copy needed",
+        
+
+    },{
+        title:"Title 4",
+        dates:"December 2017",
+        content:"content about december",
+        
+
+    },{
+        title:"Title 5",
+        dates:"December 15 2017",
+        content:"IM SO EXCITED FOR CHRISTMAS",
+
+    }
+]
+function createResumeArticles() {
+    let empty = "";
+    for (var i=0; i < resumeArticles.length; i++) {
+    let resumeArticle = `<div class="card"><h2>Title: ${resumeArticles[i].title}</h2> <h3>Date: ${resumeArticles[i].date}</h3> <p>${resumeArticles[i].content}</p></div>`;
+    empty += resumeArticle;
+    };
+    document.querySelector(".resume").innerHTML = empty;
+};
+
+createResumeArticles();
