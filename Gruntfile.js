d4cfcfd8305df90fa335af63ad2e714d4e7568bc
Courtney Brothers <courtneywbrothers@gmail.com>
2018-01-16 05:56:55 -0600

    grunt-contrib is deprecated unse only indiv dep

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,17 +19,29 @@ module.exports = function(grunt) {
           browserify: true
         }
       },
+      sass: {
+        dist: {
+          files: {
+            "stylesheets/main.css": "sass/main.scss"
+          }
+        }
+      },
       watch: {
         javascripts: {
           files: ['./javascripts/**/*.js'],
           tasks: ['jshint', 'browserify']
         },
+        sass: {
+          files: ["sass/**/*.scss"],
+          tasks: ["sass"]
+        },
+        
         hbs: {
           files: ['./templates/**/*.hbs']
         }
       }
     });
     require('matchdep').filter('grunt-*').forEach(grunt.loadNpmTasks);
-    grunt.registerTask('default', ['jshint', 'browserify', 'watch']);
+    grunt.registerTask('default', ['jshint',"sass", 'browserify', 'watch']);
    };
    
