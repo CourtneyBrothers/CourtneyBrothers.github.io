f5bbb624e196ad042ab31dc5e583d46922328e7e
Courtney Brothers <courtneywbrothers@gmail.com>
2018-01-16 17:02:43 -0600

    project page

--- javascripts/blogposts.js
+++ javascripts/blogposts.js
@@ -1,17 +1,16 @@
-"use strict";
+// "use strict";
 
-module.exports.blogData =() => {
-    let preMsg = null;
-    const blogReq = new XMLHttpRequest();
-    blogReq.addEventListener("load", parseMsg);
-    blogReq.open("GET", "blog-posts.json");
-    blogReq.send(); 
-};
+// module.exports.blogData =() => {
+//     let preMsg = null;
+//     const blogReq = new XMLHttpRequest();
+//     blogReq.addEventListener("load", parseMsg);
+//     blogReq.open("GET", "blog-posts.json");
+//     blogReq.send(); 
+// };
 
-const parseMsg = () => {
-    const blogData = JSON.parse(event.target.responseText).title;
-    for (let i = 0; i < blogData.length; i++) {
-        let arrayWithJson = messageController.addNewMessage(blogData[i]); 
-        output.updateDom(arrayWithJson);
-    }      
-};
+// const parseMsg = () => {
+//     const blogData = JSON.parse(event.target.responseText).title;
+//     for (let i = 0; i < blogData.length; i++) {
+       
+//     }      
+// };
