0dfa16b3b06d6cdd697710abf9418634e094a05b
Courtney Brothers <courtneywbrothers@gmail.com>
2018-01-16 14:07:42 -0600

    begin personal blog 4

---
+++ javascripts/blogposts.js
@@ -0,0 +1,17 @@
+"use strict";
+
+module.exports.blogData =() => {
+    let preMsg = null;
+    const blogReq = new XMLHttpRequest();
+    blogReq.addEventListener("load", parseMsg);
+    blogReq.open("GET", "blog-posts.json");
+    blogReq.send(); 
+};
+
+const parseMsg = () => {
+    const blogData = JSON.parse(event.target.responseText).title;
+    for (let i = 0; i < blogData.length; i++) {
+        let arrayWithJson = messageController.addNewMessage(blogData[i]); 
+        output.updateDom(arrayWithJson);
+    }      
+};
