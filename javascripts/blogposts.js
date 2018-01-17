cd81cd3272ee1feac720fd3216efa1a425738b8c
Courtney Brothers <courtneywbrothers@gmail.com>
2018-01-16 18:30:57 -0600

    cards for blog page with js for loop

--- javascripts/blogposts.js
+++ javascripts/blogposts.js
@@ -1,4 +1,4 @@
-// "use strict";
+"use strict";
 
 // module.exports.blogData =() => {
 //     let preMsg = null;
@@ -14,3 +14,48 @@
        
 //     }      
 // };
+
+ let blogs = [
+    {
+        title:"first blog",
+        content: "Fail forward",
+        date:"12/8/2018"
+    },{
+        title:"first blog",
+        content: "Fail forward",
+        date:"12/8/2018"
+    },{
+        title:"first blog",
+        content: "Fail forward",
+        date:"12/8/2018"
+    },{
+        title:"first blog",
+        content: "Fail forward",
+        date:"12/8/2018"
+    },{
+        title:"first blog",
+        content: "Fail forward",
+        date:"12/8/2018"
+    }
+];
+
+function createBlogCards() {
+    let empty = "";
+    for (let i=0; i < blogs.length; i++) {
+    let blogCards = `<div> <a href="" class="card><div></div><article><h1>${blogs[i].title}${blogs[i].date}</h1> <p>${blogs[i].content}</p></article></a></div>`;
+    empty += blogCards;
+    }
+    document.querySelector(".blog").innerHTML = empty;
+}
+createBlogCards();
+// <!-- <div id="card-1">
+// <a href="" class="card">
+//   <div class="thumb"></div>
+//   <article>
+//     <h1>BLOG 12/8/2017</h1>
+//     <p> "Fail Forward"</p>
+//     <p>-Joe Shepherd</p>
+
+//   </article>
+// </a> -->
+// <!-- </div> -->
--- javascripts/second.js
+++ javascripts/second.js
@@ -1,28 +1,37 @@
-// "use strict";
+"use strict";
 
-// let resumeArticles = [
-//     {
-//         title: "Freelance graphic artist",
-//         dates:"2016-present",
-//         content:"m. Ut gravida urna dolor, nec ornare lorem mollis sit amet. Curabitur vel mi ac nunc porta pretium. In vestibulum laoreet fermentum. Etiam magna orci, feugiat eget venenatis mollis, pulvinar et libero. Duis dolor nunc, vehicula eu lectus vel, interdum venenatis felis. Maecenas eu semper lacus.",
-        
+let blogs = [
+    {
+        title:"first blog",
+        content: "Fail forward",
+        date:"12/8/2018"
+    },{
+        title:"first blog",
+        content: "Fail forward",
+        date:"12/8/2018"
+    },{
+        title:"first blog",
+        content: "Fail forward",
+        date:"12/8/2018"
+    },{
+        title:"first blog",
+        content: "Fail forward",
+        date:"12/8/2018"
+    },{
+        title:"first blog",
+        content: "Fail forward",
+        date:"12/8/2018"
+    }
+];
 
-//     },{
-//         title:"Artist Director of House of Bread",
-//         dates:"2016-present",
-//         content:"“A man's work is nothing but this slow trek to rediscover, through the detours of art, those two or three great and simple images in whose presence his heart first opened.― Albert Camus",
-        
+function createBlogCards() {
+    console.log("in function");
+    let blog = document.getElementById("blog");
+    console.log(blog);
 
-//     }
-// ];
-
-// function createResumeArticles() {
-//     let empty = "";
-//     for (var i=0; i < resumeArticles.length; i++) {
-//     let resumeArticle = `<div class="card"><h2>Title: ${resumeArticles[i].title}</h2> <h3>Date: ${resumeArticles[i].date}</h3> <p>${resumeArticles[i].content}</p></div>`;
-//     empty += resumeArticle;
-//     }
-//     document.querySelector(".resume").innerHTML = empty;
-// }
-
-// createResumeArticles();
+    for (let i=0; i < blogs.length; i++) {
+        console.log("for loop");
+        blog.innerHTML += `<div> <a href="" class="card"><div></div><article><h1>${blogs[i].title}${blogs[i].date}</h1> <p>${blogs[i].content}</p></article></a></div>`;
+    }
+}
+createBlogCards();
